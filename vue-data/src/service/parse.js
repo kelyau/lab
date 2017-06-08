5791de5aab804743275d67ef6997167c1f28c3e1
kelyau <newallah@gmail.com>
2017-06-08 11:23:31 +0800

    'abc'

--- vue-data/src/service/parse.js
+++ vue-data/src/service/parse.js
@@ -1,7 +1,7 @@
 import Parse from 'parse';
 
 Parse.initialize('123456');
-Parse.serverURL = 'http://portal.xiandusi.com:1337/parse';
+Parse.serverURL = 'http://portal.xiandusi.com:1337/example';
 /* eslint-disable */
 window.Parse = Parse;
 export default Parse;
