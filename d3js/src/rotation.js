f13154bbb383d9dd7ae10ec9099d514ca2315057
kelyau <newallah@gmail.com>
2017-06-22 20:43:37 +0800

    rotation

--- d3js/src/rotation.js
+++ d3js/src/rotation.js
@@ -6,18 +6,30 @@ export default function rotation() {
 	  height: 400,
 	  scale: 180
   }
-  const canvas = d3.select('body')
-    .append('canvas')
+  const context = d3.select('body')
+    .append('svg')
 	.attr('width', box.width)
 	.attr('height', box.height)
 	.style('display', 'block')
-	.style('margin', '40px auto')
+	.style('margin', '20px auto')
 	
-  const context = canvas.node().getContext('2d');
-  
-  const sphere = {type: 'Sphere'}
-  const graticule = d3.geoGraticule()()
-  
-  d3.timer((elapsed) => {
-  })
+var projection = d3.geoAzimuthalEqualArea()
+    .scale(	80)
+    .translate([box.width / 2, box.height / 2])
+    .precision(0.1);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+var graticule = d3.geoGraticule();
+
+	
+context.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path)
+	.style('fill', 'none')
+	.style('stroke', '#777777')
+	.style('stroke-width', '1px')
+	.style('stroke-opacity', '0.5')
 }
