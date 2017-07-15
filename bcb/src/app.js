7c3d25ff5281418d7eae96414e9a7b02f5437a43
kenlyau <newallah@gmail.com>
2017-07-15 15:49:34 +0800

    bcb init

---
+++ bcb/src/app.js
@@ -0,0 +1,8 @@
+import Backbone from 'backbone'
+import MainView from './components/main'
+
+export default {
+  run(){
+    new MainView()
+  }
+}
---
+++ bcb/src/components/input.js
@@ -0,0 +1,8 @@
+import Backbone from 'backbone'
+import { h, create } from 'virtual-dom'
+
+export default function(){
+  return (
+    <input type="text" />
+  )
+}
